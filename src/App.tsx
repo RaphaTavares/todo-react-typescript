d0ba3c15c9adf330fd346547c7a2278907561158
Raphael Tavares <raphael_tavares2000@hotmail.com>
2021-10-09 15:43:38 -0300

    Initial commit (wip)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as C from './App.styles'
+import { useState } from 'react';
+import { Item } from './types/item';
+import { ListItem } from './components/ListItem';
+const App = () => {
 
-function App() {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'comprar pão', done: false },
+    { id: 2, name: 'comprar bolo', done: false }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+
+        { /* area de add nova task */}
+
+        <ul>
+          {list.map((item, index) => (
+            <ListItem key={index} item={item} />
+          ))}
+        </ul>
+      </C.Area>
+    </C.Container>
   );
 }
 
