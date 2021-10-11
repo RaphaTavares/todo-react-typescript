d46b82fc79f0a116ef6726b00755fd2b4931aeac
Raphael Tavares <raphael_tavares2000@hotmail.com>
2021-10-11 01:04:29 -0300

    Refactor checkbox state

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,16 @@ const App = () => {
     setList(newList);
   }
 
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+    for (let i in newList) {
+      if (newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
+
   return (
     <C.Container>
       <C.Area>
@@ -29,7 +39,7 @@ const App = () => {
 
         <ul>
           {list.map((item, index) => (
-            <ListItem key={index} item={item} />
+            <ListItem key={index} item={item} onChange={handleTaskChange} />
           ))}
         </ul>
       </C.Area>
